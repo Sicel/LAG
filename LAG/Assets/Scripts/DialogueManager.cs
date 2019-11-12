a079d79706f5dd30b3aac9c18b21048f6fba15be
Cristian <cristiandc10@live.com>
2019-11-12 02:06:24 -0500

    Started Dialogue System

---
+++ LAG/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DialogueManager : MonoBehaviour
+{
+    public Queue<string> textToDisplay;
+    public static DialogueManager self;
+
+    private void Awake()
+    {
+        self = this;
+    }
+
+    private void Start()
+    {
+        textToDisplay = new Queue<string>();
+    }
+
+    public void AddText(List<string> text)
+    {
+
+    }
+
+    public void DisplayText()
+    {
+
+    }
+}
