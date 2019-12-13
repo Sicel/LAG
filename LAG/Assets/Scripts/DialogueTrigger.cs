4649c3a3f1487868125b969423d7cca1c8e9bd6a
Cristian <cristiandc10@live.com>
2019-12-13 17:12:11 -0500

    Added more dialogue options

--- LAG/Assets/Scripts/DialogueTrigger.cs
+++ LAG/Assets/Scripts/DialogueTrigger.cs
@@ -6,14 +6,29 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     [SerializeField] private GameEvent gameEvent = null;
+    [SerializeField] bool triggerOnce = false;
+    [SerializeField] bool canTrigger = false;
+    [SerializeField] DialogueTrigger nextTrigger;
 
     public void OnEnable()
     {
+        gameObject.GetComponent<GameEventListener>().enabled = true;
         gameEvent.Raise();
+        gameObject.GetComponent<GameEventListener>().enabled = false;
+        enabled = false;
+
+        if (triggerOnce)
+            gameObject.GetComponent<Collider>().enabled = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (!canTrigger)
+            return;
+
+        if (other.tag != "Player")
+            return;
+
         enabled = true;
     }
 }
